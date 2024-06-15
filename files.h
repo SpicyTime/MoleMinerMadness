715c8f33d885332db264792257bca8c3bcdf7e84
SpicyTime <maximuskatsoulis985@gmail.com>
2024-06-14 21:49:20 -0500

    Delete files.h

--- files.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "config.h"
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <string>
-using namespace std;
-using namespace sf;
